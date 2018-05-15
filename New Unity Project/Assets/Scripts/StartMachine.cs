82726bd9210b806e7d43d5b3e96dc84fd7b8768c
Kane Orgill <S5069824@bournemouth.ac.uk>
2018-05-15 13:36:54 +0100

    Final commit of Tuesday

---
+++ New Unity Project/Assets/Scripts/StartMachine.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMachine : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start ()
+    {
+        SceneManager.LoadScene("TheMachine", LoadSceneMode.Additive);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
