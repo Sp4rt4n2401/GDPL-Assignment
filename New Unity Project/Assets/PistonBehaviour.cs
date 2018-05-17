1615082a477931af7b6e43126bf5011b7f3345e6
Kane Orgill <S5069824@bournemouth.ac.uk>
2018-05-18 00:01:33 +0100

    consitent spinners, dominos + piston

---
+++ New Unity Project/Assets/PistonBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PistonBehaviour : MonoBehaviour
+{
+    public float rSpeed = 0;
+    public GameObject piston;
+	// Use this for initialization
+
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        piston = GameObject.FindGameObjectWithTag("Piston");
+        if(Bunce.collisionCheck)
+        {
+            gameObject.transform.Translate(0, rSpeed, 0);
+
+            if ((gameObject.transform.position.x) > 1.5)
+            {
+                rSpeed = 0;
+            }
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Mechanisms/Bunce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bunce : MonoBehaviour
+{
+    public static bool collisionCheck;
+    public float rSpeed = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+    }
+
+    public void OnTriggerEnter(Collider PistonTrigger)
+    {
+        collisionCheck = true;
+         //PistonTrigger.transform.Translate(0, rSpeed, 0);
+    }
+}
+
